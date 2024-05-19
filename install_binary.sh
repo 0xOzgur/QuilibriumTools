ba89e5bcd4953399919b403e994d4b8571726a8b
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2024-05-19 14:41:46 +0300

    Some small changes

--- install_binary.sh
+++ install_binary.sh
@@ -3,7 +3,7 @@
 
 # Step 0: Welcome
 echo "This script is prepared by 0xOzgur.eth"
-echo "Enjoy and sit back while you are building your Quilibrium Node!"
+echo "Enjoy and sit back while you are building your Quilibrium Ceremony Client!"
 echo "Processing..."
 sleep 10  # Add a 10-second delay
 
@@ -47,7 +47,7 @@ EOF
 
 # Start the ceremonyclient service
 echo "Starting Ceremonyclient Service"
-sleep 1  # Add a 1-second delay
+sleep 2  # Add a 2-second delay
 systemctl enable ceremonyclient
 service ceremonyclient start
 

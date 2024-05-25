c2f5b473fef23035764c3bb767a43834e0d66da2
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2024-05-25 10:58:34 +0300

    docker update script added

---
+++ updateDocker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Step 0: Welcome
+echo "This script is made with ❤️ by 0xOzgur.eth"
+echo "⏳Enjoy and sit back while you are upgrading your Quilibrium Node to v1.4.18!"
+echo "⏳Processing..."
+sleep 10  # Add a 10-second delay
+
+# Stop the ceremonyclient service
+service ceremonyclient stop
+
+# Step 1:Download Binary
+echo "⏳ Downloading New Release v1.4.18"
+cd  ~/ceremonyclient
+git pull
+git checkout release
+
+# Step 7:Build Docker Container
+echo "⏳Building Ceremonyclient Container"
+sleep 2  # Add a 2-second delay
+docker build --build-arg GIT_COMMIT=$(git log -1 --format=%h) -t quilibrium -t quilibrium:1.4.18 .
+
+# Step 8:Run Ceremonyclient Container
+echo "✅Running Ceremonyclient Container"
+sleep 2  # Add a 2-second delay
+docker compose up -d
+
+# Step 9:Logs Ceremonyclient Container
+echo "🎉Welcome to Quilibrium Ceremonyclient"
+echo "⏳Please let it flow node logs at least 5 minutes then you can press CTRL + C to exit the logs."
+sleep 5  # Add a 5-second delay
+docker compose logs -f -n, --tail 100

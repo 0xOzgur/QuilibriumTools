d7aa8ac8c82a21e7c16f1ec87d1985c93f400f53
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2024-05-26 14:24:00 +0300

    added go and grpcurl installation

--- install/install_quilibrium_service.sh
+++ install/install_quilibrium_service.sh
@@ -31,6 +31,54 @@ else
 fi
 sudo sysctl -p
 
+# Installing Go 1.20.14
+wget https://go.dev/dl/go1.20.14.linux-amd64.tar.gz
+sudo tar -xvf go1.20.14.linux-amd64.tar.gz || { echo "Failed to extract Go! Exiting..."; exit_message; exit 1; }
+sudo mv go /usr/local || { echo "Failed to move go! Exiting..."; exit_message; exit 1; }
+sudo rm go1.20.14.linux-amd64.tar.gz || { echo "Failed to remove downloaded archive! Exiting..."; exit_message; exit 1; }
+
+
+# Step 4: Set Go environment variables
+echo "⏳Setting Go environment variables..."
+sleep 5  # Add a 5-second delay
+
+# Check if GOROOT is already set
+if grep -q 'GOROOT=/usr/local/go' ~/.bashrc; then
+    echo "GOROOT already set in ~/.bashrc."
+else
+    echo 'GOROOT=/usr/local/go' >> ~/.bashrc
+    echo "GOROOT set in ~/.bashrc."
+fi
+
+# Check if GOPATH is already set
+if grep -q "GOPATH=$HOME/go" ~/.bashrc; then
+    echo "GOPATH already set in ~/.bashrc."
+else
+    echo "GOPATH=$HOME/go" >> ~/.bashrc
+    echo "GOPATH set in ~/.bashrc."
+fi
+
+# Check if PATH is already set
+if grep -q 'PATH=$GOPATH/bin:$GOROOT/bin:$PATH' ~/.bashrc; then
+    echo "PATH already set in ~/.bashrc."
+else
+    echo 'PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+    echo "PATH set in ~/.bashrc."
+fi
+
+# Source .bashrc to apply changes
+echo "⏳Sourcing .bashrc to apply changes"
+source ~/.bashrc
+sleep 5  # Add a 5-second delay
+
+# Check GO Version
+go version
+sleep 5  # Add a 5-second delay
+
+# Install gRPCurl
+echo "⏳Installing gRPCurl"
+sleep 1  # Add a 1-second delay
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
 
 # Step 8:Download Ceremonyclient
 echo "⏳Downloading Ceremonyclient"

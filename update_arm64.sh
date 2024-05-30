dbb7064a8e4a8927a1cff2de206c71605f9eab3c
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2024-05-30 15:31:04 +0300

    added arm64 update for scalept

---
+++ update_arm64.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Step 0: Welcome
+echo "This script is made with ❤️ by https://quilibrium.space @ 0xOzgur.eth for scalepod.io"
+sleep 2  # Add a 2-second delay
+
+# Stop the ceremonyclient service
+service ceremonyclient stop
+
+# Step 1:Download Binary
+cd  ~/ceremonyclient
+git pull
+git checkout release
+
+# Get the current user's home directory
+HOME=$(eval echo ~$HOME_DIR)
+
+# Use the home directory in the path
+NODE_PATH="$HOME/ceremonyclient/node"
+EXEC_START="$NODE_PATH/node-1.4.18-linux-amd64"
+
+# Step 3:Re-Create Ceremonyclient Service
+rm /lib/systemd/system/ceremonyclient.service
+sudo tee /lib/systemd/system/ceremonyclient.service > /dev/null <<EOF
+[Unit]
+Description=Ceremony Client Go App Service
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+WorkingDirectory=$NODE_PATH
+ExecStart=$EXEC_START
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: Update the ExecStart line in the Ceremonyclient Service file
+# sudo sed -i "s|ExecStart=.*|ExecStart=$EXEC_START|" /lib/systemd/system/ceremonyclient.service
+
+# Step 4:Start the ceremonyclient service
+sudo systemctl daemon-reload
+sudo systemctl enable ceremonyclient
+sudo service ceremonyclient start

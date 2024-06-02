6cf9a1617c8315851a231d5d03e7e68df9881c08
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2024-06-02 04:29:18 +0300

    Q Inc Bootstrap Peers

--- visibility_check.sh
+++ visibility_check.sh
@@ -10,15 +10,6 @@ bootstrap_peers=(
 "EiB75ZnHtAOxajH2hlk9wD1i9zVigrDKKqYcSMXBkKo4SA=="
 "EiDEYNo7GEfMhPBbUo+zFSGeDECB0RhG0GfAasdWp2TTTQ=="
 "EiCzMVQnCirB85ITj1x9JOEe4zjNnnFIlxuXj9m6kGq1SQ=="
-"EiD1ClZn/lr+n/gnS96Q4gKwBDk3yl33kNIhx9wUxJiyEA=="
-"EiD7C6nsh1456MTlTihUrDqO4tLn/wcb1HXM5V7P5GXEQ=="
-"EiDUezzC3QCy6pNVg6+L9l0rH6CVIQSqxEKAGMSQJqnO1g=="
-"EiDLg4I2+SAV7f0dUfT/qwDJWAstv1CAYmbhvJG3LxrgZw=="
-"EiALqqXPhdT+LWlx2cbx3vYiFGxeUhv+KgyhE+MT8g7fLg=="
-"EiAhM5Eo0QoOQVPCJqrDCC+qe+Kx3kGohU5q+1rb5voD7A=="
-"EiC3Li+DpEtPnomK9KWzAWtW7XUzVKUewNVrURiKSeWsfg=="
-"EiBaq9Jsbu2vW+rFVrqI288t298TJUy+/xv8IF3V8uN1Xg=="
-"EiAkOMWQQAGlDDINXrhSJmnwi0LVdkFv1wLGtrp3FrYxPg=="
 )
 
 # Run the grpcurl command and capture its output

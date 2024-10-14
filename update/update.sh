7183808a57b8ef3f84d499fa1742b2544f371a77
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2024-10-14 13:34:26 +0300

    node path

--- update/update.sh
+++ update/update.sh
@@ -174,7 +174,7 @@ HOME=$(eval echo ~$HOME_DIR)
 
 # Use the home directory in the path
 NODE_PATH="$HOME/ceremonyclient/node"
-EXEC_START="$NODE_PATH/release_autorun.sh"
+EXEC_START="$NODE_PATH/$NODE_PATH"
 
 # Step 6:Create Ceremonyclient Service
 echo "⏳ Creating Ceremonyclient Service"

802ba45c34d3ffdd534c236baf097452f577b8c8
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2024-10-17 08:02:15 +0300

    v2.0.0.5

--- quilibrium_for_dummies.sh
+++ quilibrium_for_dummies.sh
@@ -7,8 +7,8 @@ clear
 
 # Set the version number
 
-VERSION="2.0.0.4"
-qClientVERSION="2.0.0.4"
+VERSION="2.0.0.5"
+qClientVERSION="2.0.0.5"
 
 
 # Determine the ExecStart line based on the architecture
